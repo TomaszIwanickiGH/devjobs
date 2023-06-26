bb1cb6e60419e7cfb85e167498a5e706b553ff79
Tomasz <tomasz.iwanicki@onet.pl>
2023-06-26 19:03:13 +0200

    Make navbar responsive

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <nav style={{ backgroundImage: 'url(assets/desktop/bg-pattern-header.svg)' }} className="rounded-es-[3.5rem] bg-no-repeat bg-cover">
-      <div className="px-40 pt-8 pb-12 flex justify-between items-center">
+    <nav style={{ backgroundImage: 'url(assets/desktop/bg-pattern-header.svg)' }} className="rounded-es-[5rem] bg-no-repeat bg-cover">
+      <div className="lg:px-40 md:px-16 px-8 pt-6 pb-12 flex justify-between items-center">
         <img src="assets/desktop/logo.svg" alt="" />
         <div className="flex gap-4 justify-center items-center py-4 rounded-md">
           <img src="assets/desktop/icon-sun.svg" alt="icon" />
