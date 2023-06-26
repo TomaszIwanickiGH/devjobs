f738c78983eee6c3637c4fb8bfd1005c84b40ead
Tomasz <tomasz.iwanicki@onet.pl>
2023-06-26 18:48:23 +0200

    Import google font and add navbar for the layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,22 @@
 import '@styles/globals.css'
+import { Kumbh_Sans } from 'next/font/google'
+import Nav from '@components/Nav'
 
 export const metadata = {
   title: 'DevJobs',
   description: 'Find your dream job in IT',
 }
 
+const kumbh = Kumbh_Sans({ subsets: ['latin'] })
+
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
-        <main>{children}</main>
+        <main className={kumbh.className}>
+          <Nav />
+          {children}
+        </main>
       </body>
     </html>
   )
