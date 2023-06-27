d18344e5dccb3408035f581f10802c32875e04a3
Tomasz <tomasz.iwanicki@onet.pl>
2023-06-27 19:52:42 +0200

    Import SearchBar and Jobs components

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,13 @@
+import SearchBar from '@components/SearchBar'
+import Jobs from '@components/Jobs'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <main className="lg:px-40 md:px-16 px-8 flex flex-col">
+      <SearchBar />
+      {/* <Jobs /> */}
+    </main>
+  )
 }
 
 export default Home
